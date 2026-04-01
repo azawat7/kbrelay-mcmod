aa22e3abb60202bcb99353756a1d1b5b17ee0823
azawat <az4wat@gmail.com>
2026-04-01 18:05:34 +0200

    Mod: Extract glfwKey to GlfwKeys.kt and fix key name mappings

--- src/main/kotlin/com/azawat/kbrelay/InputHandler.kt
+++ src/main/kotlin/com/azawat/kbrelay/InputHandler.kt
@@ -41,30 +41,4 @@ class InputHandler {
         }
     }
 
-    private fun glfwKey(key: String): Int? = when (key) {
-        "W"           -> GLFW.GLFW_KEY_W
-        "A"           -> GLFW.GLFW_KEY_A
-        "S"           -> GLFW.GLFW_KEY_S
-        "D"           -> GLFW.GLFW_KEY_D
-        "Space"       -> GLFW.GLFW_KEY_SPACE
-        "LeftShift"   -> GLFW.GLFW_KEY_LEFT_SHIFT
-        "LeftControl" -> GLFW.GLFW_KEY_LEFT_CONTROL
-        "E"           -> GLFW.GLFW_KEY_E
-        "Q"           -> GLFW.GLFW_KEY_Q
-        "F"           -> GLFW.GLFW_KEY_F
-        "Escape"      -> GLFW.GLFW_KEY_ESCAPE
-        "D1", "1"     -> GLFW.GLFW_KEY_1
-        "D2", "2"     -> GLFW.GLFW_KEY_2
-        "D3", "3"     -> GLFW.GLFW_KEY_3
-        "D4", "4"     -> GLFW.GLFW_KEY_4
-        "D5", "5"     -> GLFW.GLFW_KEY_5
-        "D6", "6"     -> GLFW.GLFW_KEY_6
-        "D7", "7"     -> GLFW.GLFW_KEY_7
-        "D8", "8"     -> GLFW.GLFW_KEY_8
-        "D9", "9"     -> GLFW.GLFW_KEY_9
-        "NumPad0", "Insert"    -> GLFW.GLFW_KEY_KP_0
-        "NumPadDecimal", "Delete" -> GLFW.GLFW_KEY_KP_DECIMAL
-        "NumPadEnter", "Return", "Enter" -> GLFW.GLFW_KEY_KP_ENTER
-        else          -> null
-    }
 }
